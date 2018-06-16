04eeb000b5f67ec897de1812550ddbc5b2fb85e8
mudskipper3000 <sasank1020@gmail.com>
2018-06-16 14:21:52 -0700

    Add files via upload
    -Fixed the Reaction Text Bug
    -Added channel creation and deletion

--- amadeus.js
+++ amadeus.js
@@ -19,36 +19,50 @@ bot.on("message", (message) => {
   if(message.author.bot) return; //if the user who wrote the command is a bot, ignore it
   if(message.content.indexOf(config.prefix) !== 0) return; //doesn't start with an !, therefore not a command
 
-  let args = message.content.slice(config.prefix.length).trim().split(/ +/g); //collects all of the data
+  let args = message.content.slice(config.prefix.length).trim().split(/ +/g); //collects all of the user inputted data (except for the first character)
   console.log(args); //prints all of the data collected
-  let command = args.shift().toLowerCase();
+  let command = args.shift().toLowerCase(); //command is the first argument
+
   if(command === 'ping') { //just a test case
     message.channel.send('pong');
   }
+
   if(command === 'react') { //takes in the user's question followed by a question
                             //ie. !react how was your day?
-
     var words = "";
-    //ERROR: doesn't get arguments
-    for(var i = 1; i < (args.length-2); i++) { //everything after the command becomes the question
-      words.concat(args[i]);
-      words.concat(" ");
+
+    while(args && args.length) { //runs as long as the array 'args' is not empty
+      words = words + args.shift() + " ";
     }
-    console.log(`words = ${words}`); //TEST: delete after bug is fixed
 
     const embed = new Discord.RichEmbed() //creating a Rich Embed
       .setTitle(`${message.author.username}'s Poll`)
       .setImage(`${message.author.displayAvatarURL}`)
-      .setDescription({words});
+      .setDescription(words);
 
     message.channel.send({embed}) //sending the embed as a message,
     .then(function (message) { //and reacting to the Rich Embed for the Poll
       message.react(thumbsUp);
       message.react(thumbsDown);
-    }).catch(function() { //error
-      console.log("REACTION ERROR");
+    }).catch(function() { //error check
+      console.error;
     });
   }
+
+  if(command === 'newchannel') { //Format is !newchannel <channel_type> <channel_name>
+      let server = message.guild;
+      let type = args.shift(); //type of channel is either text or voice
+      let name = args.shift(); //name of new channel
+      server.createChannel(name, "text");
+      console.log("new "+type+" channel \'"+name+"\' created.");
+  }
+
+  if(command === 'delchannel') {
+    message.channel.delete() //deletes the channel where the command was sent
+    .then(console.log('Deleted a channel'))
+    .catch(console.error);
+  }
+
 })
 
 function getRandomInt(min, max) { //gets a random number between min and max
